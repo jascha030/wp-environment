f473a7932b9550ad2575d8bac1a958f86d435ee7
Jascha030 <contact@jaschavanaalst.nl>
2021-09-27 21:38:25 +0200

    Add override file for users

---
+++ config/php-cs-fixer.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * PHP CS Fixer Settings
+ */
+return [
+    /**
+     * Directories Paths (relative to public/app/) to include when the `composer run format` command is executed.
+     */
+    'dirs' => [
+        // 'themes/my-wp-theme',
+    ],
+    /**
+     * Overrides for php-cs-fixer rules.
+     */
+    'rules' => [
+        // 'array_indentation' => false,
+    ],
+];
